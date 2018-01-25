3ffa207d14d6f0dcc370e5ed24fd882e1f3c8de9
Tomasz Borsukiewicz <tomasz.borsukiewicz@gmail.com>
2018-01-25 01:32:41 +0100

    ...

--- src/main/java/eu/badgerlabs/springboot2webapp1/SpringBoot2Webapp1Application.java
+++ src/main/java/eu/badgerlabs/springboot2webapp1/SpringBoot2Webapp1Application.java
@@ -2,11 +2,18 @@ package eu.badgerlabs.springboot2webapp1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class SpringBoot2Webapp1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot2Webapp1Application.class, args);
+	}
+
+	@Bean
+	HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
 	}
 }
--- src/main/java/eu/badgerlabs/springboot2webapp1/service/ImageService.java
+++ src/main/java/eu/badgerlabs/springboot2webapp1/service/ImageService.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.FileSystemUtils;
@@ -31,18 +32,31 @@ public class ImageService {
     public Flux<Image> findAllImages() {
         try {
             return Flux.fromIterable(Files.newDirectoryStream(Paths.get(UPLOAD_ROOT)))
-                    .map(path -> new Image( "" + path.hashCode(), path.getFileName().toString()));
-        }
-        catch (IOException e) {
+                    .map(path -> new Image("" + path.hashCode(), path.getFileName().toString()));
+        } catch (IOException e) {
             return Flux.empty();
         }
     }
 
     public Mono<Resource> findOneImage(String filename) {
-        return Mono.fromSupplier(() -> resourceLoader.getResource("file:" + UPLOAD_ROOT + "/" + filename));
+        return Mono.fromSupplier( () ->
+                resourceLoader.getResource("file:" + UPLOAD_ROOT + "/" + filename)
+        );
     }
 
-    
+    public Mono<Void> createImage(Flux<FilePart> files) {
+        return files.flatMap(file -> file.transferTo(Paths.get(UPLOAD_ROOT, file.filename()).toFile())).then();
+    }
+
+    public Mono<Void> deleteImage(String filename) {
+        return Mono.fromRunnable(() -> {
+            try {
+                Files.deleteIfExists(Paths.get(UPLOAD_ROOT, filename));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
 
     @Bean
